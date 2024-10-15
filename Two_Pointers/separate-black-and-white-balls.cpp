4e418a5b707af4cf09c0fa78b81ff3ee9ac7991c
Anjul Singhal <anjulsinghal123@gmail.com>
2024-10-15 23:52:53 +0530

    Day-30

---
+++ Two_Pointers/separate-black-and-white-balls.cpp
@@ -0,0 +1,17 @@
+// Leetcode 2938
+// https://leetcode.com/problems/separate-black-and-white-balls/
+
+class Solution {
+public:
+    long long minimumSteps(string s) {
+        long long swap = 0;
+        int black = 0; 
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '0') 
+                swap += (long long) black; 
+            else
+                black++; 
+        }
+        return swap;
+    }
+};
