1963d0e40e5b6c7a826340febfb6cfc4a8285b1a
Anjul Singhal <anjulsinghal123@gmail.com>
2024-10-18 23:44:57 +0530

    Day-31

---
+++ Binary_Search/search-in-rotated-sorted-array.cpp
@@ -0,0 +1,27 @@
+// Leetcode 33. search-in-rotated-sorted-array
+// https://leetcode.com/problems/search-in-rotated-sorted-array/
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int low = 0;
+        int high = n-1;
+
+        while(low <= high){
+            int mid = (low + high)/2;
+
+            if(nums[mid] == target) return mid;
+
+            if(nums[low] <= nums[mid]){
+                if(nums[low] <= target && target <= nums[mid]) high = mid - 1;
+                else low = mid + 1;
+            }
+            else{
+                if(nums[mid] <= target && target <= nums[high]) low = mid + 1;
+                else high = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
