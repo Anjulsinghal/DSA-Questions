ab5ef1e2527a1704a4df1b0e2af0bcb71104b742
Anjul Singhal <anjulsinghal123@gmail.com>
2024-11-06 01:31:04 +0530

    Day-2

---
+++ Hashing/top-k-frequent-elements.cpp
@@ -0,0 +1,24 @@
+// Leetcode 347 top-k-frequent-elements
+// https://leetcode.com/problems/top-k-frequent-elements/
+
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) 
+    {
+        vector<int>ans;
+        map<int,int>mp;
+        for(int i=0;i<nums.size();i++)
+            mp[nums[i]]++;
+        vector<pair<int,int>>v;
+        for(auto it : mp )
+            v.push_back(make_pair(it.second,it.first));
+        sort(v.rbegin(),v.rend());
+        for(int i=0;i<v.size() && k!=0 ;i++)
+        {
+            ans.push_back(v[i].second);
+            k--;
+        }       
+        return ans; 
+    }
+};
